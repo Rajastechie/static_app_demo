67285164735be08ee8d58315229ebc0d632db444
rtuser <bireshkhandekar@gmail.com>
2024-08-08 12:50:28 +0530

    fetching video done

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import YTvideos from "./ytvideos";
+
+function App() {
+  return (
+    <div className="App">
+      <YTvideos/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ytvideos.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+const API = "AIzaSyC1NL0x8Xh2Li9JOfaORmG0igyR22c81X0"
+const channelId = "UCW7UI89XzGf6N58iSyLXgmQ"
+
+var fetchurl = `https://www.googleapis.com/youtube/v3/search?key=${API}&channelId=${channelId}&part=snippet,id&order=date&maxResults=20`
+
+const YTvideos = () => {
+    const [allvideos, setAllvideos] = useState([])
+    useEffect(()=>{
+        fetch(fetchurl).then((response)=> response.json()).then((resJson)=>{
+            const result = resJson.items.map(doc=>({
+                ...doc,
+                VideoLink: "https://www.youtube.com/embed/"+doc.id.videoId
+            }));
+            setAllvideos(result)
+        })
+    },[])
+
+    console.log(allvideos)
+    return (
+        <div>
+            {allvideos.map((item)=>{
+                return(
+                    <div>
+                        <iframe width="560" height="315" src={item.VideoLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default YTvideos
